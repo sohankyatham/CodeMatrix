a569adcf61f5b66ce2f2ca9cf58ca6fb1b97db95
Sohan Kyatham <45246932+sohankyatham@users.noreply.github.com>
2021-03-10 12:26:45 -0500

    Create TemplateManager.py

---
+++ Code-Knight/TemplateManager.py
@@ -0,0 +1,76 @@
+# File Templates for Code Knight
+
+
+
+# HTML FILE TEMPLATES 
+
+
+
+# HTML Basic Markup
+HTML_Basic_Markup = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+</head>
+
+<body>
+    
+</body>
+</html>
+"""
+
+
+
+# HTML Login Form
+HTML_LoginForm = """<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+	<meta charset="UTF-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Untitled</title> 
+</head> 
+<body> 
+	<form> 
+		<div> 
+			<label>Username</label> 
+			<input> 
+		</div> 
+
+		<div> 
+
+			<label>Password</label> 
+			<input> 
+		</div> 
+
+		<button>Submit</button> 
+	</form> 
+</body> 
+</html>
+"""
+
+
+
+# PYTHON FILE TEMPLATES
+
+
+
+# Tkinter Start Code
+Tkinter_StartCode = """# Imports
+from tkinter import *
+
+
+
+# Main Window 
+root = Tk()
+root.geometry("600x600")
+root.title("Untitled")
+
+
+
+# Mainloop
+root.mainloop()
+"""
