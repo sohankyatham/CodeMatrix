74af8427e25f945e2ed5a3e39ce328ce58da7491
Sohan Kyatham <45246932+sohankyatham@users.noreply.github.com>
2021-03-24 12:46:48 -0400

    Add More File Templates

---
+++ Code-Knight/FileTemplates.py
@@ -0,0 +1,40 @@
+# Basic File Templates for Code Knight
+
+
+
+# Basic HTML Markup Template
+Basic_HTML_Markup = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+</head>
+
+<body>
+    
+</body>
+</html>
+"""
+
+Basic_PHP_File = """
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+</head>
+
+<body>
+    
+</body>
+</html>
+"""
